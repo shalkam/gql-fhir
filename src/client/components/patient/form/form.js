617d707b184b4e17737186c92ba1f712f2891efc
shalkam <shalkamm@gmail.com>
2017-01-30 16:36:57 +0200

    changed queries in the client to match the server

--- src/client/components/patient/form/form.js
+++ src/client/components/patient/form/form.js
@@ -2,27 +2,28 @@ import React, { Component, PropTypes } from 'react';
 import Formsy from 'formsy-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import {hashHistory} from 'react-router';
+import { hashHistory } from 'react-router';
+import { Tab, Nav, NavItem, Button, Col, Row } from 'react-bootstrap';
+import { fromObj } from 'form-data-to-object';
 import {
-  Tab,
-  Nav,
-  NavItem,
-  Button,
-  Col,
-  Row
-} from 'react-bootstrap';
-import {fromObj} from 'form-data-to-object';
-import { Checkbox, CheckboxGroup, Input, RadioGroup, Select, File, Textarea } from 'formsy-react-components';
+  Checkbox,
+  CheckboxGroup,
+  Input,
+  RadioGroup,
+  Select,
+  File,
+  Textarea
+} from 'formsy-react-components';
 import ToHumanName from '../../../common/fhir/helpers/to-human-name.js';
 import ToNationalID from '../../../common/fhir/helpers/to-national-id.js';
 
 class Form extends Component {
   componentWillMount() {
-    this.setState({canSubmit: false, model: {}});
+    this.setState({ canSubmit: false, model: {} });
   }
-  componentWillReceiveProps(newProps){
+  componentWillReceiveProps(newProps) {
     let model = newProps.model;
-    if(model.deceasedDateTime) {
+    if (model.deceasedDateTime) {
       const date = new Date(model.deceasedDateTime);
       model.deceasedDateTime = date.toISOString().slice(0, -1);
     }
@@ -32,175 +33,198 @@ class Form extends Component {
   formatDateTime(datetime) {
     datetime = new Date(datetime);
     datetime = datetime
-    .toLocaleString('en-US',{hour12: false})
-    .replace(/\//g, '-')
-    .replace(', ', 'T');
+      .toLocaleString('en-US', { hour12: false })
+      .replace(/\//g, '-')
+      .replace(', ', 'T');
     return datetime;
   }
   enableButton() {
-    this.setState({
-      canSubmit: true
-    });
+    this.setState({ canSubmit: true });
   }
   disableButton() {
-    this.setState({
-      canSubmit: false
-    });
+    this.setState({ canSubmit: false });
   }
   submit(model) {
     // if(model.name) model.name = ToHumanName(model.name);
     // if(model.identifier) model.identifier = ToNationalID(model.identifier);
     console.log(model);
-    this.props.mutate({ variables: {model}})
-    .then(({ data }) => {
-      new Notification("Patient Saved", {body: `Patient ${model.name[0].text} was saved`})
-      hashHistory.push('/patients');
-      console.log('got data', data);
-    }).catch((error) => {
-      console.log('there was an error sending the query', error);
-    });
+    this.props
+      .mutate({ variables: { model } })
+      .then(({ data }) => {
+        new Notification('Patient Saved', { body: `Patient ${model.name[0].text} was saved` });
+        hashHistory.push('/patients');
+        console.log('got data', data);
+      })
+      .catch(error => {
+        console.log('there was an error sending the query', error);
+      });
   }
   render() {
-    return <Formsy.Form ref='form' onValidSubmit={this.submit.bind(this)} onValid={this.enableButton.bind(this)} onInvalid={this.disableButton.bind(this)}>
-      <Tab.Container defaultActiveKey="first">
-        <div>
-          <Nav bsStyle="tabs">
-            <NavItem eventKey="first">
-              Tab 1
-            </NavItem>
-            <NavItem eventKey="second">
-              Tab 2
-            </NavItem>
-          </Nav>
-          <div className="padded-more">
-            <Tab.Content animation>
-              <Tab.Pane eventKey="first">
-                <Input name="_id" value={null} type='hidden'/>
-                <Input name="identifier[0][value]" label="National ID" required/>
-                <Checkbox name="active" label="active?" value={false}/>
-                <Input name="name[0][text]" label="Patient's Name" required/>
-                <fieldset>
-                  <legend>Telecom</legend>
-                  <Select
-                    name="telecom[0][system]"
-                    label="System"
-                    help="This is a required select element."
-                    options={[
-                      {value: '', label: 'Select...'},
-                      {value: 'Phone', label: 'phone'},
-                      {value: 'Fax', label: 'fax'},
-                      {value: 'Email', label: 'email'},
-                      {value: 'Pager', label: 'pager'},
-                      {value: 'URL', label: 'URL'}
-                    ]}
-                    required
-                    />
-                  <Select
-                    name="telecom[0][use]"
-                    label="Use"
-                    help="This is a required select element."
-                    options={[
-                      {value: '', label: 'Select...'},
-                      {value: 'Home', label: 'Home'},
-                      {value: 'Work', label: 'Work'},
-                      {value: 'Temp', label: 'Temp'},
-                      {value: 'Old', label: 'Old'},
-                      {value: 'Mobile', label: 'Mobile'}
-                    ]}
-                    required
-                    />
-                  <Input name="telecom[0][value]" label="Value" required/>
-                </fieldset>
-                <Select
-                  name="gender"
-                  label="Gender"
-                  options={[
-                    {value: '', label: 'Select...'},
-                    {value: 'Male', label: 'Male'},
-                    {value: 'Female', label: 'Female'},
-                    {value: 'Other', label: 'Other'},
-                    {value: 'Unknown', label: 'Unknown'}
-                  ]}
-                  required
-                  />
-              </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                <div className="padded-more">
-                  <Input name='birthDate' label='Birth Date' type="date"/>
-                  <Checkbox name='deceasedBoolean' label='is deceased?' value={false} />
-                  <Input name='deceasedDateTime' label='Deceased date and time' type="datetime-local"/>
+    return (
+      <Formsy.Form
+        ref="form"
+        onValidSubmit={this.submit.bind(this)}
+        onValid={this.enableButton.bind(this)}
+        onInvalid={this.disableButton.bind(this)}
+      >
+        <Tab.Container defaultActiveKey="first">
+          <div>
+            <Nav bsStyle="tabs">
+              <NavItem eventKey="first">
+                Tab 1
+              </NavItem>
+              <NavItem eventKey="second">
+                Tab 2
+              </NavItem>
+            </Nav>
+            <div className="padded-more">
+              <Tab.Content animation>
+                <Tab.Pane eventKey="first">
+                  <Input name="_id" value={null} type="hidden" />
+                  <Input name="identifier[0][value]" label="National ID" required />
+                  <Checkbox name="active" label="active?" value={false} />
+                  <Input name="name[0][text]" label="Patient's Name" required />
                   <fieldset>
-                    <legend>Address</legend>
+                    <legend>Telecom</legend>
                     <Select
-                      name="address[0][use]"
+                      name="telecom[0][system]"
+                      label="System"
+                      help="This is a required select element."
+                      options={[
+                        { value: '', label: 'Select...' },
+                        { value: 'Phone', label: 'phone' },
+                        { value: 'Fax', label: 'fax' },
+                        { value: 'Email', label: 'email' },
+                        { value: 'Pager', label: 'pager' },
+                        { value: 'URL', label: 'URL' }
+                      ]}
+                      required
+                    />
+                    <Select
+                      name="telecom[0][use]"
                       label="Use"
                       help="This is a required select element."
                       options={[
-                        {value: '', label: 'Select...'},
-                        {value: 'Home', label: 'Home'},
-                        {value: 'Office', label: 'Office'},
-                        {value: 'Temporary', label: 'Temporary'},
-                        {value: 'OldIncorrect', label: 'OldIncorrect'}
+                        { value: '', label: 'Select...' },
+                        { value: 'Home', label: 'Home' },
+                        { value: 'Work', label: 'Work' },
+                        { value: 'Temp', label: 'Temp' },
+                        { value: 'Old', label: 'Old' },
+                        { value: 'Mobile', label: 'Mobile' }
                       ]}
-                      />
-                    <Textarea name="address[0][text]" label="text" />
+                      required
+                    />
+                    <Input name="telecom[0][value]" label="Value" required />
                   </fieldset>
                   <Select
-                    name="mariatalStatus[text]"
-                    label="Mariatal Status"
+                    name="gender"
+                    label="Gender"
                     options={[
-                      {value: '', label: 'Select...'},
-                      {value: 'A', label: 'Annulled'},
-                      {value: 'D', label: 'Divorced'},
-                      {value: 'I', label: 'Interlocutory'},
-                      {value: 'L', label: 'Legally Separated'},
-                      {value: 'M', label: 'Married'},
-                      {value: 'P', label: 'Polygamous'},
-                      {value: 'S', label: 'Never Married'},
-                      {value: 'T', label: '	Domestic partner'},
-                      {value: 'W', label: 'Widowed'},
-                      {value: 'UNK', label: 'Unknown'}
+                      { value: '', label: 'Select...' },
+                      { value: 'Male', label: 'Male' },
+                      { value: 'Female', label: 'Female' },
+                      { value: 'Other', label: 'Other' },
+                      { value: 'Unknown', label: 'Unknown' }
                     ]}
+                    required
+                  />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <div className="padded-more">
+                    <Input name="birthDate" label="Birth Date" type="date" />
+                    <Checkbox name="deceasedBoolean" label="is deceased?" value={false} />
+                    <Input
+                      name="deceasedDateTime"
+                      label="Deceased date and time"
+                      type="datetime-local"
                     />
-                  <Checkbox name='multipleBirthBoolean' label='is multiple birth?' value={false} />
-                  <Input name='multipleBirthInteger' label='Number of birth' type="number" step="1"/>
-                </div>
-              </Tab.Pane>
-            </Tab.Content>
-            <Button className='pull-right' bsStyle="primary" type="submit" disabled={!this.state.canSubmit}>
-              Submit
-            </Button>
+                    <fieldset>
+                      <legend>Address</legend>
+                      <Select
+                        name="address[0][use]"
+                        label="Use"
+                        help="This is a required select element."
+                        options={[
+                          { value: '', label: 'Select...' },
+                          { value: 'Home', label: 'Home' },
+                          { value: 'Office', label: 'Office' },
+                          { value: 'Temporary', label: 'Temporary' },
+                          { value: 'OldIncorrect', label: 'OldIncorrect' }
+                        ]}
+                      />
+                      <Textarea name="address[0][text]" label="text" />
+                    </fieldset>
+                    <Select
+                      name="mariatalStatus[text]"
+                      label="Mariatal Status"
+                      options={[
+                        { value: '', label: 'Select...' },
+                        { value: 'A', label: 'Annulled' },
+                        { value: 'D', label: 'Divorced' },
+                        { value: 'I', label: 'Interlocutory' },
+                        { value: 'L', label: 'Legally Separated' },
+                        { value: 'M', label: 'Married' },
+                        { value: 'P', label: 'Polygamous' },
+                        { value: 'S', label: 'Never Married' },
+                        { value: 'T', label: '	Domestic partner' },
+                        { value: 'W', label: 'Widowed' },
+                        { value: 'UNK', label: 'Unknown' }
+                      ]}
+                    />
+                    <Checkbox
+                      name="multipleBirthBoolean"
+                      label="is multiple birth?"
+                      value={false}
+                    />
+                    <Input
+                      name="multipleBirthInteger"
+                      label="Number of birth"
+                      type="number"
+                      step="1"
+                    />
+                  </div>
+                </Tab.Pane>
+              </Tab.Content>
+              <Button
+                className="pull-right"
+                bsStyle="primary"
+                type="submit"
+                disabled={!this.state.canSubmit}
+              >
+                Submit
+              </Button>
+            </div>
           </div>
-        </div>
-      </Tab.Container>
-    </Formsy.Form>
+        </Tab.Container>
+      </Formsy.Form>
+    );
   }
 }
-Form.propTypes = {
-  mutate: PropTypes.func.isRequired
-};
+Form.propTypes = { mutate: PropTypes.func.isRequired };
 
 const submit = gql`
 mutation submit($model: PatientInput!) {
-  upsertPatient(data: $model) {
-    _id
-    active
-    birthDate
-    identifier {
-      value
+  patient {
+    upsert(data: $model) {
+      _id
+      active
+      birthDate
+      identifier {
+        value
+      }
+      name {
+        text
+      }
+      address {
+        use
+      }
+      telecom {
+        use
+        system
+        value
+      }
+      gender
     }
-    name {
-      text
-    }
-    address {
-      use
-    }
-    telecom {
-      use
-      system
-      value
-    }
-    gender
   }
 }`;
 
--- src/client/components/patient/list/index.js
+++ src/client/components/patient/list/index.js
@@ -1,51 +1,63 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import {hashHistory} from 'react-router';
-import {Button} from 'react-bootstrap';
+import { hashHistory } from 'react-router';
+import { Button } from 'react-bootstrap';
 
 class List extends Component {
   componentWillReceiveProps(newProps) {
     this.props.data.refetch();
   }
   render() {
-    if(!this.props.data.loading) {
-      const {patients} = this.props.data
-      return <table className="table-striped">
-        <thead>
-          <tr>
-            <th></th>
-            <th>id</th>
-            <th>Name</th>
-            <th>Name use</th>
-          </tr>
-        </thead>
-        <tbody>
-          {patients.map((patient) => {
-            return <tr>
-              <td>
-                <Button bsSize="xsmall" bsStyle='primary' onClick={() => hashHistory.push('patients/'+patient._id+'/edit')}>
-                  edit
-                </Button>
-              </td>
-              <td>{patient._id}</td>
-              <td>{patient.name[0].text}</td>
-              <td>{patient.name[0].use}</td>
+    if (!this.props.data.loading) {
+      const { patients } = this.props.data;
+      return (
+        <table className="table-striped">
+          <thead>
+            <tr>
+              <th />
+              <th>id</th>
+              <th>Name</th>
+              <th>Name use</th>
             </tr>
-          })}
-        </tbody>
-      </table>;
+          </thead>
+          <tbody>
+            {patients.map(patient => {
+              return (
+                <tr>
+                  <td>
+                    <Button
+                      bsSize="xsmall"
+                      bsStyle="primary"
+                      onClick={() => hashHistory.push('patients/' + patient._id + '/edit')}
+                    >
+                      edit
+                    </Button>
+                  </td>
+                  <td>{patient._id}</td>
+                  <td>{patient.name[0].text}</td>
+                  <td>{patient.name[0].use}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      );
     }
     return <div>Loading ...</div>;
   }
 }
-var component = graphql(gql`query showAll {
-  patients {
-    _id
-    name {
-      text
-      use
+var component = graphql(
+  gql`query showAll {
+  patient {
+    find{
+      _id
+      name {
+        text
+        use
+      }
     }
   }
-}`)(List);
+}`
+)(List);
 export default component;
